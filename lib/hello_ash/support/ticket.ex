3e5f054e0cd2c59681fa1b30124df3fc45d893b0
Andreas Stocker <stoand@protonmail.com>
2023-12-24 11:30:25 +0100

    ticket: custom actions

--- lib/hello_ash/support/ticket.ex
+++ lib/hello_ash/support/ticket.ex
@@ -1,12 +1,29 @@
 defmodule HelloAsh.Support.Ticket do
-    use Ash.Resource
+  use Ash.Resource
 
-    actions do
-        defaults [:create, :read, :update, :destroy]
+  actions do
+    defaults [:create, :read, :update, :destroy]
+
+    create :open do
+        accept [:subject]
+    end
+    
+    create :open_with_status do
+        accept [:subject, :status]
+    end
+  end
+
+  attributes do
+    uuid_primary_key :id
+
+    attribute :subject, :string do
+      allow_nil? false
     end
 
-    attributes do
-        uuid_primary_key :id
-        attribute :subject, :string
+    attribute :status, :atom do
+        constraints [one_of: [:open, :closed]]
+        default :open
+        allow_nil? false
     end
+  end
 end
